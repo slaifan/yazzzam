1e1dba2c5b9caf781a5d4c9155c857003cd7dd8c
Sumair Rehman Paracha <58272909+sumairrehman99@users.noreply.github.com>
2022-02-23 12:46:43 +0000

    Update QueryController.java

--- Server/src/main/java/yazzam/server/Controllers/QueryController.java
+++ Server/src/main/java/yazzam/server/Controllers/QueryController.java
@@ -5,16 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@Controller
-//public class GreetingController {
-//
-//    @GetMapping("/query")
-//    public String query(@RequestParam(song="song", required=true) String song, Model model) {
-//        model.addAttribute("song", song);
-//        return "query";
-//    }
-//
-//}
+
 
 @Controller
 public class QueryController {
