85f4f30fee2977a38d56134ce13aae08c37f6b38
ZayRex <31854984+ZayRex@users.noreply.github.com>
2022-03-05 04:20:16 +0000

    Imported soundex and metaphone phonetic algorithms

--- Index/src/main/java/uk/ac/ed/yazzzam/Preprocessor/FullProcessor.java
+++ Index/src/main/java/uk/ac/ed/yazzzam/Preprocessor/FullProcessor.java
@@ -1,35 +1,39 @@
 package uk.ac.ed.yazzzam.Preprocessor;
 
 import opennlp.tools.stemmer.PorterStemmer;
+import org.apache.commons.codec.language.Metaphone;
+import org.apache.commons.codec.language.Soundex;
 import uk.ac.ed.yazzzam.Indexer.Reader;
 import uk.ac.ed.yazzzam.Indexer.TextFileReader;
 
 import java.util.*;
-import java.util.function.Predicate;
 
 public class FullProcessor implements Preprocessor {
 
 
 	private PorterStemmer ps;
 	private Reader r;
+	private Soundex sound;
+	private Metaphone mp;
 	Set<String> stopwords;
 
 	public FullProcessor(String stopWordsFileName)  {
 		ps = new PorterStemmer();
 		r = new TextFileReader();
+		sound = new Soundex();
+		mp = new Metaphone();
 		this.stopwords = new HashSet<>(Arrays.asList(r.readFile(stopWordsFileName).split(" ")));
 	}
 
 
-	
 	// Tokenizes a string at every non-alphanumeric character
 	private List<String> tokenize(String document) {
 		var tokens = new ArrayList<String>();
 		document = document.replaceAll("\\s+"," ");
 		var split_docs = document.split("[^\\w]+");
-		
-		for (int i = 0; i < split_docs.length; i++) {
-			tokens.add(split_docs[i]);
+
+		for (String split_doc : split_docs) {
+			tokens.add(split_doc);
 		}
 		
 		return tokens;
@@ -56,12 +60,36 @@ public class FullProcessor implements Preprocessor {
 		return tokens;
 	}
 
+	private List<String> soundex(ArrayList<String> tokens){
+		for (int i = 0; i < tokens.size(); i++) {
+			tokens.set(i, sound.encode(tokens.get(i)));
+		}
+		return tokens;
+	}
+
+	private List<String> metaphone(ArrayList<String> tokens){
+		for (int i = 0; i < tokens.size(); i++) {
+			tokens.set(i, mp.encode(tokens.get(i)));
+		}
+		return tokens;
+	}
+
+	@Override
 	public List<String> preprocess(String text) {
 		return stem(removeStopWords(tokenize(caseFold(text))));
 	}
-	
-	
-	
+
+	public List<String> preprocess(String text, String wordReducAlg) {
+
+		if (wordReducAlg.equals("stem"))
+			return stem(removeStopWords(tokenize(caseFold(text))));
+		else if (wordReducAlg.equals("soundex"))
+			return soundex(removeStopWords(tokenize(caseFold(text))));
+		else if (wordReducAlg.equals("metaphone"))
+			return metaphone(removeStopWords(tokenize(caseFold(text))));
+		return stem(removeStopWords(tokenize(caseFold(text))));
+	}
+
 	
 
 	
--- Index/src/main/java/uk/ac/ed/yazzzam/Preprocessor/Preprocessor.java
+++ Index/src/main/java/uk/ac/ed/yazzzam/Preprocessor/Preprocessor.java
@@ -3,5 +3,6 @@ package uk.ac.ed.yazzzam.Preprocessor;
 import java.util.List;
 
 public interface Preprocessor {
-    public List<String> preprocess(String text);
+    List<String> preprocess(String text);
+    List<String> preprocess(String text, String wordReducAlg);
 }
