466c3ca8638098f8c0d2266025c657ceb7043998
slaifan <fassouleh200@gmail.com>
2022-03-08 14:55:34 +0000

    fix conflicts

--- Index/src/main/java/uk/ac/ed/yazzzam/Main.java
+++ Index/src/main/java/uk/ac/ed/yazzzam/Main.java
@@ -3,6 +3,8 @@ package uk.ac.ed.yazzzam;
 import uk.ac.ed.yazzzam.Indexer.CSVReader;
 import uk.ac.ed.yazzzam.Indexer.Song;
 import uk.ac.ed.yazzzam.Preprocessor.BasicPreprocessor;
+import uk.ac.ed.yazzzam.Preprocessor.FullProcessor;
+import uk.ac.ed.yazzzam.Ranker.BM25;
 import uk.ac.ed.yazzzam.Search.PhraseSearch;
 import uk.ac.ed.yazzzam.Indexer.IndexBuilder;
 import uk.ac.ed.yazzzam.index.InvertedIndex;
@@ -22,7 +24,6 @@ public class Main {
         System.out.println(memoryState());
 
         Long startProcessDocs = System.nanoTime();
-        IndexBuilder ib = new IndexBuilder();
         var i = 0;
         while (songsIter.hasNext()){
             var song = songsIter.next();
@@ -35,47 +36,22 @@ public class Main {
         Long endProcessDocs = System.nanoTime();
         System.out.println("processing and indexing took: " + getTimeSeconds(startProcessDocs, endProcessDocs) + " seconds");
         System.out.println(memoryState());
-//        var idx = ib.getIndex();
-//        System.out.println(idx);
-//        System.out.println(ib.getDocLengths());
 
-//        var invertedIndex = new ProximityInvertedIndex(idx);
+        var preprocessor = new FullProcessor("englishST.txt");
 
-//        var similarity = new SimilaritySearch(invertedIndex.inverted_index.keySet()); // to test change visibility of MapBasedInvertedIndex.invertedIndex to public
+        var ranker = new BM25(ib);
+        var q1 = "laptop in my back pocket";
+        var prec_q = preprocessor.preprocess(q1);
+        var out = ranker.score(prec_q);
+        System.out.println(out);
 
-//        var indexWriter = new BinaryProximityIndexWriter();
-//        indexWriter.writeToFile(invertedIndex, "test");
-//
-//        var indexDiskReader = new FullPostingListDiskReader();
-//        var reconstructedInvertedIndex = new CompressedProximityInvertedIndex(indexDiskReader.readAll("index/test.ii"));
-//
-//        indexWriter.writeToFile(reconstructedInvertedIndex, "test2");
 
-//        var x = invertedIndex.getPostingList("my");
-//        System.out.println(x);
-//        var q1 = "laptop in my back pocket";
-//        var q2 = "I";
-//        var q3 = "I have something in my pocket";
-//
-//        Long startSearch = System.nanoTime();
-//
-//        System.out.println(similarity.findSimilarWords("odin", 2));
-//
-//        testSearch(q1, invertedIndex);
-//        testSearch(q2, invertedIndex);
-//        testSearch(q3, invertedIndex);
-//
-//        Long endSearch = System.nanoTime();
-//
-//        System.out.println("searching took: " + getTimeSeconds(startSearch, endSearch)+ " seconds");
+
+        //System.out.println(idx);
+        //System.out.println(ib.getDocLengths());
+
     }
 
-    private static void testSearch(String query, InvertedIndex invertedIndex) {
-        var prec = new BasicPreprocessor();
-        var q = prec.preprocess(query);
-        var res = PhraseSearch.Search(q, invertedIndex);
-        System.out.println(res);
-    }
 
     private static Double getTimeSeconds(Long start, Long end) {
         Long durationNano = end - start;
